7154fc71c0d61c8ee90bb5377fb1bc346d39cb1f
Mychaell <emmaway3@gmail.com>
2023-10-10 11:46:53 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include "dog.h"
+#include <stdlib.h>
+#include "dog.h"
 
+/**
+ * print_dog -  a function that prints a struct dog
+ *
+ * @d: struct dog
+ * Return: 0
+ */
 
-	/**
-	 * print_dog: a function that prints a struct dog
-	 *
-	 * @d: struct dog
-	 * Return: 0
-	 */
-	void print_dog(struct dog *d)
-	{
-		if (d == NULL)
-			return;
-
-
-		if (d->name == NULL)
-			d->name = "(nil)";
-		if (d->owner == NULL)
-			d->owner = "(nil)";
-
-
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-	}
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,103 +1,97 @@
 #include <stdlib.h>
-	#include "dog.h"
+#include "dog.h"
 
+/**
+ * _strlen - function that returns the length
+ * @s: string
+ *
+ * Return: string length
+ */
 
-	/**
-	 * _strlen - function that returns the length
-	 * @s: string
-	 *
-	 * Return: string length
-	 */
-	int _strlen(char *s)
+int _strlen(char *s)
+{
+	int i;
+	
+	i = 0;
+	
+	while (s[i] != '\0')
 	{
-		int i;
-
-
-		i = 0;
-
-
-		while (s[i] != '\0')
-		{
-			i++;
-		}
-
-
-		return (i);
+		i++;
 	}
+	
+	return (i);
+}
 
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string
+ *
+ * Return: the pointer
+ */
 
-	/**
-	 * *_strcpy - copies the string pointed to by src
-	 * @dest: pointer to the buffer in which we copy the string
-	 * @src: string
-	 *
-	 * Return: the pointer
-	 */
-	char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+	len = 0;
+	
+	while (src[len] != '\0')
 	{
-		int len, i;
-
-
-		len = 0;
-
-
-		while (src[len] != '\0')
-		{
-			len++;
-		}
-
-
-		for (i = 0; i < len; i++)
-		{
-			dest[i] = src[i];
-		}
-		dest[i] = '\0';
-
-
-		return (dest);
+		len++;
 	}
-
-
-	/**
-	 * new_dog - a function that creates a new dog
-	 * @name: dog's name
-	 * @age: dog's age
-	 * @owner: dog's owner
-	 *
-	 * Return: pointer to the new dog (Success), NULL otherwise
-	 */
-	dog_t *new_dog(char *name, float age, char *owner)
+	
+	for (i = 0; i < len; i++)
 	{
-		dog_t *dog;
-		int len1, len2;
-
-
-		len1 = _strlen(name);
-		len2 = _strlen(owner);
-
-
-		dog = malloc(sizeof(dog_t));
-		if (dog == NULL)
-			return (NULL);
-
-
-		dog->name = malloc(sizeof(char) * (len1 + 1));
-		if (dog->name == NULL)
-		{
-			free(dog);
-			return (NULL);
-		}
-		dog->owner = malloc(sizeof(char) * (len2 + 1));
-		if (dog->owner == NULL)
-		{
-			free(dog);
-			free(dog->name);
-			return (NULL);
-		}
-		_strcpy(dog->name, name);
-		_strcpy(dog->owner, owner);
-		dog->age = age;
-
-
-		return (dog);
+		dest[i] = src[i];
 	}
+	dest[i] = '\0';
+	
+	return (dest);
+}
+
+/**
+ * new_dog - a function that creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+	
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+	
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		
+		return (NULL);
+	
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	
+	_strcpy(dog->name, name);
+	
+	_strcpy(dog->owner, owner);
+	
+	dog->age = age;
+	
+	return (dog);
+}
