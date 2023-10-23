0a1d473d6030eeeb85604952b7cb11cc8c333d0d
Mychaell <emmaway3@gmail.com>
2023-10-23 07:04:15 -0700

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - func to calculate the sum of all the data in the  list
+ * @head: first node
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
